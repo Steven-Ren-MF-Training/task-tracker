a50499abee21cff69d06aaf71d85582a59724013
Steven Ren MF <renkaifeng777@gmail.com>
2026-07-29 13:40:46 +0930

    feature/TC-2-more-exercise

---
+++ exercise_03.py
@@ -0,0 +1,25 @@
+# Exercise 3 - Display Data Types
+
+# Create variables with different data types
+task_name = "Design Homepage"
+task_id = 201
+estimated_hours = 8.5
+task_completed = False
+
+# Display each value and its type
+
+# Display task_name's value and type
+print("Task Name:", task_name)
+print(type(task_name))
+
+# Display Task_id's value and type
+print("Task ID:", task_id)
+print(type(task_id))
+
+# Display Estimated_Hours's value and type
+print("Estimated Hours:", estimated_hours)
+print(type(estimated_hours))
+
+# Display Completion status's value and type
+print("Task Completed:", task_completed)
+print(type(task_completed))
---
+++ exercise_07.py
@@ -0,0 +1,9 @@
+# Ask the user to enter project information
+project_name = input("Enter Project Name: ")
+team_leader = input("Enter Team Leader: ")
+team_members = int(input("Enter Number of Team Members: "))
+
+# Display the project information
+print("Project Name:", project_name)
+print("Team Leader:", team_leader)
+print("Team Members:", team_members)
